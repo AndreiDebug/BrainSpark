ce1af61eda08595519889acf13f0d3eb6d09400c
CorneliusMarian <102810808+CorneliusMarian@users.noreply.github.com>
2022-10-11 17:10:38 +0300

    all works

--- SlideShow.js
+++ SlideShow.js
@@ -1,26 +1,27 @@
-<script>
-    let slideIndex = 0;
-    showSlides();
+let slideIndex = 0;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+
+showSlides();
 
 function showSlides() {
-    let i; 
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    for(i = 0; i < slides.lenght; i++){
-        slides[i].style.disply = "none";
-    }
-    slideIndex++;
+  let i;
 
-    if(slideIndex > slides.lenght){
-        slideIndex = 1;
-    }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
 
-    for(i = 0; i < dots.lenght; i++)
-    {
-        dots[i].className = dots[i].className.replace("active", "");
-    }
-    slides[slideIndex-1].style.disply = "block";
-    dots[slideIndex-1].className += "active";
-    setTimeout(showSlides, 2000);
+  slideIndex++;
 
-</script>
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000);
+}
